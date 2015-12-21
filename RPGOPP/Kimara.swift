148e76ec1c55138a747428df8e5e648c9be30b42
ikanmakeit <tom.tkan@gmail.com>
2015-12-21 10:22:41 -0500

    Created Enemy subclass "Kimora"

---
+++ RPGOPP/Kimara.swift
@@ -0,0 +1,30 @@
+//
+//  Kimara.swift
+//  RPGOPP
+//
+//  Created by Thomas Kan on 12/21/15.
+//  Copyright © 2015 Thomas Kan. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom","Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr)
+        } else {
+            return false
+        }
+    }
+}
